51ed91b3e67fd91a1ef91819071cba0cdfb44a4d
s3rbar <barsulsergij@gmail.com>
2025-01-22 20:30:16 +0200

    Adapt account forms to different width resolutions, Add checkbox function to app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
+    // Toggle menu code
     let menuToggle = document.querySelector("div.menu-toggle");
     let navMenu = document.querySelector("#nav-menu");
 
-    menuToggle.addEventListener('click', function(){
-        menuToggle.classList.toggle('active');
-        document.body.classList.toggle('active');
-        navMenu.classList.toggle('active');
-    })
+    if(menuToggle){
+        menuToggle.addEventListener('click', function(){
+            menuToggle.classList.toggle('active');
+            document.body.classList.toggle('active');
+            navMenu.classList.toggle('active');
+        });
+    }
+
+
+    // Checkes input when user click on a 'keep me logged in' section
+    let keepLogin = document.querySelector('section.keep-login');
+    let checkbox = keepLogin.children[0];
+    
+    if(keepLogin){
+        keepLogin.addEventListener('click', function(){
+            checkbox.checked ? checkbox.checked = false : checkbox.checked = true;
+        });
+    }
 })
